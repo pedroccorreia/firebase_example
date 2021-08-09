126c417fbf689af89c75881a11cc87399c5b0eb3
Pedro Correia <pcorreia@pcorreia.altostrat.com>
2021-08-09 23:40:09 +0000

    Initial Commit

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-PROJECT_ID = 'firestore-example-322405'
-COLLECTION_NAME = u'forecastData'
-FILE_NAME = 'bom.csv'
+PROJECT_ID = '<Your Project>'
+COLLECTION_NAME = u'<Your Collection Name>'
+FILE_NAME = '<input file>'
